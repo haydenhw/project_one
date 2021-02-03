bc2b2a1a442930162a82f7e519140d27605f8a5f
hayden <hayden321@gmail.com>
2021-02-02 21:54:33 -0800

    reorders objects for code clarity

--- hotel-california/hotel-california/src/main/scala/example/Main.scala
+++ hotel-california/hotel-california/src/main/scala/example/Main.scala
@@ -4,45 +4,17 @@ import sys.process._
 import scala.language.postfixOps
 import scala.collection.mutable.ArrayBuffer
 
-case class Page(val referrer: String, val source: String, val linkCount: Int) {}
-
-object DAO {
-  def findNthGreatestLinkSource(referrer: String, n: Int): Page = {
-    println("Running query...")
-    val cmd =
-      s"""
-        hive -S -e "select * from clickstream where referrer='$referrer' order by occurrences DESC limit 1 offset $n" 
-      """
-
-    var stdout = new StringBuilder
-    val stderr = new StringBuilder
-    val status = cmd ! ProcessLogger(stdout append _, stderr append _)
-
-    val arr = stdout.toString().split('\t')
-    print(s"shell output: ")
-    arr.foreach(print)
-    println()
-
-    val source = arr(1)
-    val linkCount = arr(3).toInt
-
-    println(source, linkCount)
-
-    Page(referrer, source, linkCount)
-  }
-}
-
-
 object Main extends App {
-  if (args.length != 2) {
-    println("Usage: <page title> <combination range>")
+  if (args.length != 3) {
+    println("Usage: <page title> <path depth> <path range>")
     System.exit(-1)
   }
 
-  val n = args(1).toInt
+  val pathDepth = args(1).toInt
+  val pathRange = args(2).toInt
   val combinations = Util.permutationsWithRepetitions(
-    List.range(1, n),
-    n
+    List.range(1, pathRange + 1),
+    pathDepth
   )
 
   val referrer = args(0)
@@ -50,20 +22,18 @@ object Main extends App {
     .map(Clickstream.findPathByReferrer(referrer, _))
     .map(Clickstream.aggregatePath)
   Clickstream.printSummary(paths)
+}
 
-  val mockpaths = List(
-    List(
-      Page("foo", "bar", 100),
-      Page("bar", "baz", 200),
-      Page("baz", "bang", 300)
-    ),
-    List(
-      Page("fred", "mary", 400),
-      Page("mary", "brian", 300),
-      Page("brian", "sally", 800)
-    )
-  )
-
+object Util {
+  def permutationsWithRepetitions[T](input: List[T], n: Int): List[List[T]] = {
+    require(input.length > 0 && n > 0)
+    n match {
+      case 1 => for (el <- input) yield List(el)
+      case _ =>
+        for (el <- input; perm <- permutationsWithRepetitions(input, n - 1))
+          yield el :: perm
+    }
+  }
 }
 
 object Clickstream {
@@ -104,14 +74,28 @@ object Clickstream {
   }
 }
 
-object Util {
-  def permutationsWithRepetitions[T](input: List[T], n: Int): List[List[T]] = {
-    require(input.length > 0 && n > 0)
-    n match {
-      case 1 => for (el <- input) yield List(el)
-      case _ =>
-        for (el <- input; perm <- permutationsWithRepetitions(input, n - 1))
-          yield el :: perm
-    }
+object DAO {
+  def findNthGreatestLinkSource(referrer: String, n: Int): Page = {
+    val cmd =
+      s"""
+        hive -S -e "SELECT * FROM clickstream WHERE referrer='$referrer' ORDER BY occurrences DESC LIMIT 1 OFFSET $n" 
+      """
+
+    println(s"\nExecuting query... ${cmd}\n")
+
+    var stdout = new StringBuilder
+    val stderr = new StringBuilder
+    val status = cmd ! ProcessLogger(stdout append _, stderr append _)
+
+    val parsedOutput = stdout.toString().split('\t')
+
+    val source = parsedOutput(1)
+    val linkCount = parsedOutput(3).toInt
+
+    println(source, linkCount)
+
+    Page(referrer, source, linkCount)
   }
 }
+
+case class Page(val referrer: String, val source: String, val linkCount: Int) {}
